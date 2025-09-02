0d9cb1bf5c3fb549b6a1c187b9abe2d1198f1239
ethanepiscope <eepiscope@gmail.com>
2025-09-02 01:31:56 -0500

    1.6 done

--- ArraysAndHashing/6StringEncodeDecode.py
+++ ArraysAndHashing/6StringEncodeDecode.py
@@ -1,36 +1,23 @@
-#Original solution was pretty dumb just using split, join, and a pseudorandom
-#sequence asa delimeter. This solution feels safer to me
+#O(m) time, O(m+n) space. m is sum of lengths of all strings and n is number of strings
 
 class Solution:
 
     def encode(self, strs: List[str]) -> str:
-        #Handling edge case
-        if not strs:
-            return ""
-        
-        #Creating the encoding
-        encoding = []
-        firstLetterTracker = 0
+        ret = ""
         for s in strs:
-            encoding.extend([str(firstLetterTracker),'+'])
-            firstLetterTracker += len(s)
-        encoding[-1] = "="
-
-        #Stick the smushed string on the end
-        return ''.join(encoding) + ''.join(strs)
+            ret += str(len(s)) + ':' + s
+        return ret
 
     def decode(self, s: str) -> List[str]:
-        #edge case check
-        if s == "":
-            return []
-
-        #Isolate encodings and smushed string
-        [encodings,s] = s.split('=', maxsplit=1)
-        startPos = [int(num) for num in encodings.split('+')]
-
-        #Build return list
+        i = 0 #starts of sequences
+        j = 1 #ends of sequences
         ret = []
-        for i in range(len(startPos)-1):
-            ret.append(s[startPos[i] : startPos[i+1]])
-        ret.append(s[startPos[len(startPos)-1]:])
+        while i < len(s):
+            if s[j] != ':':
+                j += 1
+            else:
+                length = int(s[i:j])
+                ret.append(s[j+1:j+1+length])
+                i = j+1+length
+                j = i + 1
         return ret
