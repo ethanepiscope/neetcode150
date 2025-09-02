5af937345f89ada020dfdd3a7cb1be4cd967fe04
ethanepiscope <eepiscope@gmail.com>
2025-09-02 16:59:47 -0500

    2.4 done

--- TwoPointers/MaxWaterContainer.py
+++ TwoPointers/MaxWaterContainer.py
@@ -1,15 +1,16 @@
-#this was a great change of pace. O(n)
+#O(n) time, O(1) space
 
 class Solution:
     def maxArea(self, heights: List[int]) -> int:
-        l = 0
-        r = len(heights)-1
-        max = 0
-        while l < r:
-            if min(heights[l],heights[r]) * (r-l) > max:
-                max = min(heights[l],heights[r]) * (r-l)
-            if heights[l] < heights[r]:
-                l += 1
+        i = 0
+        j = len(heights) - 1
+        max_area = 0
+        while i < j:
+            area = (j-i)*min(heights[i],heights[j])
+            if area > max_area:
+                max_area = area
+            if heights[j] < heights[i]:
+                j -= 1
             else:
-                r -= 1
-        return max
+                i += 1
+        return max_area
