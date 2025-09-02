45704573ed457221fb1faf7dfba2c28e83979693
ethanepiscope <eepiscope@gmail.com>
2025-09-02 00:08:22 -0500

    completed valid anagram and edited duplicate int

--- ArraysAndHashing/1DuplicateInteger.py
+++ ArraysAndHashing/1DuplicateInteger.py
@@ -5,9 +5,9 @@
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        seen = set() #O(1)
+        seen = set()
         for num in nums: #O(n) total
             if num in seen: #O(1) per iter
-                return True #O(1) per iter
+                return True
             seen.add(num) #O(1) per iter
-        return False #O(1)
+        return False
--- ArraysAndHashing/2IsAnagram.py
+++ ArraysAndHashing/2IsAnagram.py
@@ -1,15 +1,18 @@
+# O(n + m) time complexity, O(1) space complexity since there are only 26 letters
+# I didn't realize I could just use == for dictionaries which would have saved some time
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         s_dict = {}
-        for char in s:
-            if char not in s_dict:
-                s_dict[char] = 0
-            s_dict[char] += 1
-        for char in t:
-            if char not in s_dict:
+        for c in s: #O(n)
+            if c not in s_dict.keys(): #O(1) per iter
+                s_dict[c] = 0 #O(1) per iter
+            s_dict[c] += 1 #O(1) per iter
+        for c in t: #O(m)
+            if c not in s_dict.keys(): #O(1) per iter
                 return False
-            s_dict[char] -= 1
-        for key in s_dict:
-            if s_dict[key] != 0:
+            s_dict[c] -= 1 #O(1) per iter
+        for v in s_dict.values(): #O(n)
+            if v != 0: #O(1) per iter
                 return False
         return True
