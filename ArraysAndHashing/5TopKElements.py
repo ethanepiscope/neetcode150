0086e4a6595bf8ab9f646b8da9b39e95af28c204
ethanepiscope <eepiscope@gmail.com>
2025-09-02 01:10:57 -0500

    completed 1.5

--- ArraysAndHashing/5TopKElements.py
+++ ArraysAndHashing/5TopKElements.py
@@ -1,12 +1,18 @@
-#Notes: lambda function raaaaah
+#O(n) time, O(n) space
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = {}
+        freq_dict = {} # map numbers to frequencies
+        for num in nums: #O(n)
+            freq_dict[num] = 1 + freq_dict.get(num, 0)
+        freq_buckets = [[] for num in nums] # map frequencies to lists of numbers
+        for num, freq in freq_dict.items(): #O(n)
+            freq_buckets[freq-1].append(num)
         ret = []
-        for num in nums:
-            freq[num] = 1 + freq.get(num, 0)
-        useful = sorted(freq.items(), key=lambda item: item[1], reverse=True)
-        for i in range(k):
-            ret.append(useful[i][0])
+        for num_list in freq_buckets[::-1]: #O(n) for the iteration and O(n) for the reversal
+            for num in num_list:
+                if k == 0:
+                    break
+                ret.append(num)
+                k -= 1
         return ret
