d148105bd38dba01ce07a37d7572e724cb0af3c0
ethanepiscope <eepiscope@gmail.com>
2025-09-02 16:42:33 -0500

    2.3 done

--- TwoPointers/ThreeIntegerSum.py
+++ TwoPointers/ThreeIntegerSum.py
@@ -1,24 +1,21 @@
-#roughly n^2 solution. n^3 technically because checking if the list is in ret
-#is n time. I would normally make ret a set and just pass it arrays to hash, but i
-#didn't want to import the arrays package lol. worst case is only an issue 
-#if there are a lot of combos to make zero
-
+#O(n^2) time, O(n)ish space for the output list.
+        
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ret = []
-        nums.sort()
+        nums = sorted(nums)
+        ret = set()
         for i in range(len(nums)):
-            j = i+1
+            target = nums[i] * -1
+            j = i + 1
             k = len(nums) - 1
-            while j < k:    
-                if nums[i] + nums[j] + nums[k] > 0:
-                    k -= 1
-                elif nums[i] + nums[j] + nums[k] < 0:
+            while j<k:
+                total = nums[j] + nums[k]
+                if total < target:
                     j += 1
+                elif total > target:
+                    k -= 1
                 else:
-                    if sorted([nums[i],nums[j],nums[k]]) not in ret:
-                        ret.append(sorted([nums[i],nums[j],nums[k]]))
+                    ret.add((nums[i],nums[j],nums[k]))
                     j += 1
                     k -= 1
         return list(ret)
-        
