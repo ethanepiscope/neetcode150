ee8b4af28374978734251549627a0a08dc24bd63
ethanepiscope <eepiscope@gmail.com>
2025-09-02 00:51:43 -0500

    finished 1.4

--- ArraysAndHashing/4AnagramGroups.py
+++ ArraysAndHashing/4AnagramGroups.py
@@ -1,15 +1,15 @@
-#My thoughts: python is a strange and terrifying language
+# O(mn) time, O(m) space. This is gross and would be better if I used ord() to get unicode values and a size 26 array to represent the dict.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        count_to_word = {}
-        for s in strs:
-            letterCount = {}
-            for letter in s:
-                letterCount[letter] = 1 + letterCount.get(letter, 0)
-            key = tuple(sorted(letterCount.items()))
-            if key in count_to_word:
-                count_to_word[key].append(s)
+        d = {} #map freq map tuple to list of strings
+        for s in strs: # O(m)
+            freq_map = {}
+            for c in s: #create the freq map in O(n) time
+                freq_map[c] = 1 + freq_map.get(c, 0)
+            key = tuple(sorted(freq_map.items())) #O(1) per iter since freq map has at most 26 entries
+            if key in d:
+                d[key].append(s)
             else:
-                count_to_word[key] = [s]
-        return count_to_word.values()
+                d[key] = [s]
+        return list(d.values())
