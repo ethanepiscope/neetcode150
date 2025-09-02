cce381b6da3e8d6acd406472a7ca6f56080ebde5
ethanepiscope <eepiscope@gmail.com>
2025-09-02 16:07:14 -0500

    1.9 done

--- ArraysAndHashing/9LongestConsecutiveSequence.py
+++ ArraysAndHashing/9LongestConsecutiveSequence.py
@@ -1,15 +1,28 @@
-#I couldn't think of an O(n) solution on my own so I looked at the provided solution :(
-#Initially thought there was an issue if we had many elements that were the start of a sequence,
-#but since sets don't permit duplicates this wasnt and issue
+#O(n^2) time. try next time to use hash set
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        uniqueNums = set(nums)
-        longestSeq = 0
-        for n in uniqueNums:
-            if (n - 1) not in uniqueNums:
-                currentLength = 1
-                while (n + currentLength) in uniqueNums:
-                    currentLength += 1
-                longestSeq = max(currentLength, longestSeq)
-        return longestSeq
+        d = {} #map start of sequence to end of sequence
+        for num in nums: #O(n)
+            if num+1 not in d.keys() and num-1 not in d.values(): #check if num cannot be added to start or end of existing sequence
+                if num not in d.keys() and num not in d.values(): #assuming not, make sure num isn't a duplicate
+                    d[num] = num
+            if num+1 in d.keys() and num-1 in d.values(): #num is between two sequences
+                for start in d.keys(): #O(n) sadly
+                    if d[start] == num - 1:
+                        d[start] = d[num+1]
+                        del d[num+1]
+                        break
+            if num+1 in d.keys():
+                d[num] = d[num+1]
+                del d[num+1]
+            if num-1 in d.values():
+                for start in d.keys(): #O(n) again
+                    if d[start] == num - 1:
+                        d[start] = num
+                        break
+        ret = 0
+        for start,end in d.items():
+            if end-start+1 > ret:
+                ret = end-start+1
+        return ret
