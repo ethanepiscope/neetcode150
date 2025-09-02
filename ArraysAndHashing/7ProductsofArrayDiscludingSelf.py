081bb1604b4f13234cc9783acbc4778bdbe7edf1
ethanepiscope <eepiscope@gmail.com>
2025-09-02 14:32:11 -0500

    1.7 done

--- ArraysAndHashing/7ProductsofArrayDiscludingSelf.py
+++ ArraysAndHashing/7ProductsofArrayDiscludingSelf.py
@@ -3,16 +3,12 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        prodInclAndUpTo = [nums[0]]*n #memoizes product of all nums up to and including index i
-        prodInclAndAfter = [nums[n-1]]*n #memoizes product of all nums after and including index i
-
+        preproduct = [1]*n #memoizes product of all nums up to and NOT including index i
+        postproduct = [1]*n #memoizes product of all nums after and NOT including index i
         for i in range(1,n):
-            prodInclAndUpTo[i] = nums[i] * prodInclAndUpTo[i-1]
-            prodInclAndAfter[n-1-i] = nums[n-1-i] * prodInclAndAfter[n-i]
-        
-        ret = [prodInclAndAfter[1]] * n
-        ret[n-1] = prodInclAndUpTo[n-2]
-        for i in range(1,n-1):
-            ret[i] = prodInclAndUpTo[i-1] * prodInclAndAfter[i+1]
+            preproduct[i] = preproduct[i-1] * nums[i-1]
+            postproduct[-i-1] = postproduct[-i] * nums[-i]
+        ret = [1]*n
+        for i in range(n):
+            ret[i] = preproduct[i] * postproduct[i]
         return ret
-
