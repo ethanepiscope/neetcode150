26edc1da75b0abfd40b6bdb46292957d0e478503
ethanepiscope <eepiscope@gmail.com>
2025-09-01 23:27:04 -0500

    redid duplicate integer

--- ArraysAndHashing/1DuplicateInteger.py
+++ ArraysAndHashing/1DuplicateInteger.py
@@ -1,9 +1,13 @@
-#Diorgano
-
+# O(n) time complexity, O(n) space complexity
+# Opted to return True the first time we encounter
+# a duplicate instead of just comparing list and set
+# lengths at the end.
 
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
-        g = set()
-        for num in nums:
-            g.add(num)
-        return len(g) != len(nums)
+        seen = set() #O(1)
+        for num in nums: #O(n) total
+            if num in seen: #O(1) per iter
+                return True #O(1) per iter
+            seen.add(num) #O(1) per iter
+        return False #O(1)
