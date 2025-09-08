49aaec467f44f48fc80b0848e3d197d8bfe44d9b
ethanepiscope <eepiscope@gmail.com>
2025-09-08 15:29:03 -0500

    7.2 done

---
+++ 07Trees/02MaxDepthBinTree.py
@@ -0,0 +1,15 @@
+# Recursive DFS. O(n) time, O(h) space, where h is the height of the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+        
